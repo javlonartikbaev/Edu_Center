d72befbae6b57a9277d582a5935c2f85128d8ee6
javlonartikbaev <artjavlon@gmail.com>
2024-07-16 16:29:14 +0500

    fixef settings.py media

--- Educational_Center/settings.py
+++ Educational_Center/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
